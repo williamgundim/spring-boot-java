8eb8225b26f02792b03e04339dac9f5c098ef0bc
williamgundim <williammatos09@gmail.com>
2021-08-22 21:29:53 -0300

    Criado repository

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,25 +3,31 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @Controller
 public class TopicosController {
 
+	@Autowired //Utilizado para injeção de dependencia no spring.
+	private TopicoRepository topicoRepository;
+	
 	@RequestMapping("/topicos")
 	@ResponseBody
-	public List<TopicoDTo> lista() {
+	public List<Topico> lista() {
+
+		//Curso newCurso = new Curso("Angular", "Web");
+		//Topico novoTopico = new Topico("Titulo", "Nova AAAA", newCurso);
+		//TopicoDTo novoTopicoDTO = new TopicoDTo(topicos);
 		
-		Curso newCurso = new Curso("Angular", "Web");
-		Topico novoTopico = new Topico("Titulo", "Nova AAAA", newCurso);
-		TopicoDTo novoTopicoDTO = new TopicoDTo(novoTopico);
-		
-		return Arrays.asList(novoTopicoDTO);
+		List<Topico> topicos = this.topicoRepository.findAll();
+		return topicos;
 		
 	}
 	
